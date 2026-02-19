5fca121f432aa8019489d409682cac47bd40ff5e
JagdeepSingh13 <jagdeeps3105@gmail.com>
2026-02-19 11:43:37 +0530

    config es kibana keyc mapstruct

---
+++ src/main/java/com/example/restrO/RestrOApplication.java
@@ -0,0 +1,13 @@
+package com.example.restrO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestrOApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restrO/RestrOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restrO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
